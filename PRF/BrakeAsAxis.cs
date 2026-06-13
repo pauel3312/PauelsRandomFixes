3390c1b6af0633059efe731fa423fad9a41ad1d4
pauel <pau.lacazedieu@gmail.com>
2026-06-13 15:26:03 +0200

    BrakeAsAxis is now configurable for with or without negative region.

--- PRF/BrakeAsAxis.cs
+++ PRF/BrakeAsAxis.cs
@@ -5,12 +5,26 @@ namespace PRF;
 
 [Fix]
 [HarmonyPatch]
-internal class BrakeAsAxis(ConfigFile config): ConfigurableFix(config)
+internal class BrakeAsAxis: ConfigurableFix
 {
+    public BrakeAsAxis(ConfigFile config) : base(config)
+    {
+        _useBrakesNegativeRegion = config.Bind("BrakeAsAxis", "UseBrakesNegativeRegion", false, "Use negative region of input for the brake axis");
+    }
+
+    private static ConfigEntry<bool> _useBrakesNegativeRegion = null!;
+    
     [HarmonyPatch(typeof(PilotPlayerState), nameof(PilotPlayerState.PlayerControls))]
     [HarmonyPostfix]
     public static void PlayerControlsPostfix(PilotPlayerState __instance)
     {
-        __instance.controlInputs.brake = (__instance.player.GetAxisRaw("Brake") + 1) / 2f;
+        if (_useBrakesNegativeRegion.Value)
+        {
+            __instance.controlInputs.brake = (__instance.player.GetAxisRaw("Brake") + 1) / 2f;
+        }
+        else
+        {
+            __instance.controlInputs.brake = __instance.player.GetAxisRaw("Brake");
+        }
     }
 }
