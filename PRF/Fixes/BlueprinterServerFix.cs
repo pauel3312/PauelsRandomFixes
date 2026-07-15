ba38f88729e084c5a428839d7a93f3a66208e912
Appulcake <metroid4.metroid4@gmail.com>
2026-07-15 09:52:56 +0200

    Changed BlueprinterServerFix.cs to use Prepare + TargetMethod to dynamically check for Blueprinter and its PrefabHashAssigner method's existence instead of hard relying on it, to prevent throwing errors on load if blueprinter isn't running. As a side effect, Blueprinter is no longer needed as a dependency.

--- PRF/Fixes/BlueprinterServerFix.cs
+++ PRF/Fixes/BlueprinterServerFix.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using BepInEx.Configuration;
-using Blueprinter;
 using HarmonyLib;
 using Mirage;
 using UnityEngine;
 using UnityEngine.Rendering;
+
+// ReSharper disable UnusedMember.Local
 
 namespace PRF.Fixes;
 
@@ -18,30 +21,52 @@ internal class BlueprinterServerFix(ConfigFile config) : ConfigurableFix(config)
     protected override string Description =>
         $"{base.Description}\nFixes Blueprinter's prefabHash reassignments from being reset/cleaned up before full game load is complete, fixing various prefab mix-up issues with too many BP content mods.";
     
-    [HarmonyPatch(typeof(PrefabHashAssigner), nameof(PrefabHashAssigner.AssignFromBundles))]
+    // Prepare + TargetMethod to check whether Blueprinter even exists
+    [HarmonyPrepare]
+    private static bool Prepare() => AccessTools.TypeByName("Blueprinter.PrefabHashAssigner") != null;
+    
+    [HarmonyTargetMethod]
+    private static MethodBase? TargetMethod()
+    {
+        var type = AccessTools.TypeByName("Blueprinter.PrefabHashAssigner");
+        
+        return type == null
+            ? null
+            : AccessTools.Method(type, "AssignFromBundles");
+    }
+    
     [HarmonyPostfix]
     // ReSharper disable once InconsistentNaming
-    private static void AssignFromBundlesPostfix(IReadOnlyDictionary<string, LoadedBundle> bundles)
+    private static void AssignFromBundlesPostfix(object __0)
     {
         // Check if it's running on a dedicated/headless server, no need to do this on client
         var dedicatedServer = Application.isBatchMode || SystemInfo.graphicsDeviceType == GraphicsDeviceType.Null;
         
-        if (!dedicatedServer || bundles == null)
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+        if (!dedicatedServer || __0 == null)
         {
             PRF.Logger.LogDebug("[BlueprinterServerFix] Not on dedicated server, exiting patch.");
             return;
         }
         
+        // Check whether AssignFromBundles and its LoadedBundle bundles exists/is accessible
+        var valuesProperty = AccessTools.Property(__0.GetType(), "Values");
+        
+        if (valuesProperty?.GetValue(__0) is not IEnumerable bundles)
+        {
+            PRF.Logger.LogDebug("[BlueprinterServerFix] Could not read Blueprinter bundles.");
+            return;
+        }
+        
         var savedIdentities = 0;
         
-        foreach (var loadedBundle in bundles.Values)
+        foreach (var loadedBundle in bundles)
         {
-            // Don't judge I'm trying my best to guard it from null in every way
-            
-            if (loadedBundle?.AssetBundle == null)
+            if (loadedBundle == null)
                 continue;
             
-            var assetBundle = loadedBundle?.AssetBundle;
+            var assetBundle =
+                AccessTools.Field(loadedBundle.GetType(), "AssetBundle")?.GetValue(loadedBundle) as AssetBundle;
             
             if (assetBundle == null)
                 continue;
@@ -54,10 +79,12 @@ internal class BlueprinterServerFix(ConfigFile config) : ConfigurableFix(config)
             }
             catch (Exception ex)
             {
-                // Yet it still complained about it being possibly null here
+                var filePath =
+                    AccessTools.Field(loadedBundle.GetType(), "filePath")?.GetValue(loadedBundle)?.ToString() ??
+                    "<Unknown Filepath>";
                 
                 PRF.Logger.LogError(
-                    $"[BlueprinterServerFix] Failed to save prefabs from \"{loadedBundle?.filePath}\": {ex.Message}");
+                    $"[BlueprinterServerFix] Failed to save prefabs from \"{filePath}\": {ex.Message}");
                 
                 continue;
             }
@@ -69,6 +96,8 @@ internal class BlueprinterServerFix(ConfigFile config) : ConfigurableFix(config)
                 
                 var identities = entry.GetComponentsInChildren<NetworkIdentity>(true);
                 
+                // Save all NetworkIdentities to SavedNetworkIdentities that have their prefabHash changed
+                // this keeps them from getting cleared by Unity which'd revert them to original and incorrect prefabHash
                 foreach (var identity in identities)
                 {
                     if (!identity)
@@ -85,7 +114,7 @@ internal class BlueprinterServerFix(ConfigFile config) : ConfigurableFix(config)
             }
         }
         
-        PRF.Logger.LogDebug(
+        PRF.Logger.LogInfo(
             $"[BlueprinterServerFix] Saved {savedIdentities} new NetworkIdentity prefabs; {SavedNetworkIdentities.Count} total.");
     }
 }
